c6b4b7a746fbcf8cdd6d3c4c27790f0c5611b0f1
Eric Langlois <eric@langlois.xyz>
2020-06-16 22:03:10 -0400

    Read config file to set user_config and change default data dir to in /tmp

--- spinup/user_config.py
+++ spinup/user_config.py
@@ -1,5 +1,13 @@
+import configparser
 import os
 import os.path as osp
+import tempfile
+
+CONFIG_DIR = osp.join(
+    os.getenv("XDG_CONFIG_HOME", osp.expanduser("~/.config")), "spinup"
+)
+config = configparser.ConfigParser()
+config.read(osp.join(CONFIG_DIR, "config"))
 
 # Default neural network backend for each algo
 # (Must be either 'tf1' or 'pytorch')
@@ -13,15 +21,17 @@ DEFAULT_BACKEND = {
 }
 
 # Where experiment outputs are saved by default:
-DEFAULT_DATA_DIR = osp.join(osp.abspath(osp.dirname(osp.dirname(__file__))),'data')
+DEFAULT_DATA_DIR = config.get(
+    "spinup", "data_dir", fallback=osp.join(tempfile.gettempdir(), "experiments")
+)
 
 # Whether to automatically insert a date and time stamp into the names of
 # save directories:
-FORCE_DATESTAMP = False
+FORCE_DATESTAMP = config.getboolean("spinup", "force_datestamp", fallback=False)
 
 # Whether GridSearch provides automatically-generated default shorthands:
-DEFAULT_SHORTHAND = True
+DEFAULT_SHORTHAND = config.getboolean("spinup", "default_shorthand", fallback=True)
 
 # Tells the GridSearch how many seconds to pause for before launching 
 # experiments.
-WAIT_BEFORE_LAUNCH = 5
+WAIT_BEFORE_LAUNCH = config.getint("spinup", "wait_before_launch", fallback=5)
